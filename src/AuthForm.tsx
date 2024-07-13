92fe39a653a34e6b359efde6890d9c09002926e4
unknown <exozy.nebulous@gmail.com>
2024-07-14 00:04:33 +0500

    first

--- src/AuthForm.tsx
+++ src/AuthForm.tsx
@@ -1,18 +1,48 @@
+import axios from 'axios';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import './AuthForm.css';
 import { Link } from "react-router-dom";
 export default function AuthForm() {
 
+
+interface AuthFormProps {
+  username: string;
+  password: string;
+}
+
+const {register, handleSubmit} = useForm<AuthFormProps>()
+
+const submitForm: SubmitHandler<AuthFormProps> = async data => {
+  try {
+    await axios.get('сюда апишку ставь' ///!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      , {
+      params: {
+        email: data.username,
+        password: data.password,
+      }
+    });
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('Ошибка:', error.response?.data);
+    } else {
+      console.error('Непредвиденная ошибка:', error);
+    }
+  }
+};
   return (
     <div className="container">
       <div className="form-main">
         <h2>Добро пожаловать</h2>
         <h3 className="name-doc">AlgoDoc</h3>
-        <form>
+        <form onSubmit={handleSubmit(submitForm)}>
           <div>
-            <input type="email" id="email" name="email" required placeholder="Email"/>
+          <input type="text" id="username" required placeholder="Username or Email"
+              {...register('username')}
+            />
           </div>
           <div>
-            <input type="password" id="password" name="password" required placeholder="Пароль" />
+            <input type="password" id="password" required placeholder="Пароль"
+            {...register('password')} />
           </div>
           <button className="submit" type="submit">Войти</button>
         </form>
--- src/RegForm.tsx
+++ src/RegForm.tsx
@@ -1,19 +1,56 @@
+import axios from 'axios';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import './AuthForm.css';
 import { Link } from "react-router-dom";
 export default function AuthForm() {
   
+
+  interface RegFormProps {
+    email: string;
+    password: string;
+    username: string;
+    confirmPassword: string;
+  }
+  const {register, handleSubmit} = useForm<RegFormProps>()
+
+
+  const submitForm: SubmitHandler<RegFormProps> = async data => {
+    if (data.password!== data.confirmPassword) {
+      alert('Пароли не совпадают!');
+      return;
+    }
+    try {
+      await axios.post('сюда апишку ставь' ///!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        , {
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error('Ошибка:', error.response?.data);
+      } else {
+        console.error('Непредвиденная ошибка:', error);
+      }
+    }
+  };
   return (
     <div className="container">
       <div className="form-main">
         <h2>Добро пожаловать!</h2>
         <h3 className="name-doc">AlgoDoc</h3>
-        <form>
+        <form onSubmit={handleSubmit(submitForm)}>
           <div>
-            <input type="email" id="email" name="email" required placeholder="Email"/>
+          <input type="text" id="username" required placeholder="Username"
+              {...register('username')}
+            />
+            <input type="email" id="email"required placeholder="Email"
+            {...register('email')}/>
           </div>
           <div>
-            <input type="password" id="password" name="password" required placeholder="Пароль" />
-            <input type="password" id="confirm-password" name="password" required placeholder="Подтвердите Пароль" />
+            <input type="password" id="password" required placeholder="Пароль"
+            {...register('password')}/>
+            <input type="password" id="confirm-password" required placeholder="Подтвердите Пароль"/>
           </div>
           <button className="submit" type="submit">Зарегистрироваться</button>
         </form>
