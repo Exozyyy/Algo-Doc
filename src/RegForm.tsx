5474209a5906e9107ec004a8319a4735fcd07b53
Exozy <exozy.nebulous@gmail.com>
2024-07-14 21:57:57 +0500

    form-reqs
    прописал логику формы регистрации+авторизации, добавил запросы, хеширование для паролей

--- src/RegForm.tsx
+++ src/RegForm.tsx
@@ -2,30 +2,34 @@ import axios from 'axios';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import './AuthForm.css';
 import { Link } from "react-router-dom";
-export default function AuthForm() {
-  
+import CryptoJS from 'crypto-js';
 
+export default function AuthForm() {
   interface RegFormProps {
     email: string;
     password: string;
     username: string;
-    confirmPassword: string;
+    confirmPassword: string; 
   }
-  const {register, handleSubmit} = useForm<RegFormProps>()
 
+  const { register, handleSubmit} = useForm<RegFormProps>();
 
   const submitForm: SubmitHandler<RegFormProps> = async data => {
-    if (data.password!== data.confirmPassword) {
+    if (data.password !== data.confirmPassword) {
       alert('Пароли не совпадают!');
       return;
     }
+
+    const hashedPassword = CryptoJS.SHA256(data.password).toString(CryptoJS.enc.Hex);
+
     try {
-      await axios.post('сюда апишку ставь' ///!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        , {
+      const response = await axios.post('сюда апишку ставь', {
         username: data.username,
         email: data.email,
-        password: data.password,
+        password: hashedPassword,
       });
+
+      localStorage.setItem('user', JSON.stringify(response.data));
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.error('Ошибка:', error.response?.data);
@@ -34,6 +38,7 @@ export default function AuthForm() {
       }
     }
   };
+
   return (
     <div className="container">
       <div className="form-main">
@@ -41,16 +46,17 @@ export default function AuthForm() {
         <h3 className="name-doc">AlgoDoc</h3>
         <form onSubmit={handleSubmit(submitForm)}>
           <div>
-          <input type="text" id="username" required placeholder="Username"
+            <input type="text" id="username" required placeholder="Username"
               {...register('username')}
             />
-            <input type="email" id="email"required placeholder="Email"
-            {...register('email')}/>
+            <input type="email" id="email" required placeholder="Email"
+              {...register('email')} />
           </div>
           <div>
             <input type="password" id="password" required placeholder="Пароль"
-            {...register('password')}/>
-            <input type="password" id="confirm-password" required placeholder="Подтвердите Пароль"/>
+              {...register('password')} />
+            <input type="password" id="confirm-password" required placeholder="Подтвердите Пароль"
+              {...register('confirmPassword')} />
           </div>
           <button className="submit" type="submit">Зарегистрироваться</button>
         </form>
